1382c19031cf3383137cf79ea515882369acc9fc
Trong Tin Nguyen <Trong Tin Nguyen>
2016-01-07 21:29:18 +0700

    + add new files

--- www/js/controllers/account.js
+++ www/js/controllers/account.js
@@ -1,32 +1,35 @@
 angular.module('solomo.controllers')
 
-    .controller('AccountCtrl', function($scope, UserService, $ionicActionSheet, $state, $ionicLoading){
+    .controller('AccountCtrl', function($scope, UserService, $ionicActionSheet, $state, $ionicLoading,$cordovaCamera){
+      $scope.choosePicture = function(){
+        $scope.imageHandle(Camera.PictureSourceType.PHOTOLIBRARY);
+    };
 
-        $scope.user = UserService.getUser();
+    $scope.takePicture = function(){
+        $scope.imageHandle(Camera.PictureSourceType.CAMERA);
+    };
 
-        $scope.showLogOutMenu = function() {
-            var hideSheet = $ionicActionSheet.show({
-                destructiveText: 'Logout',
-                titleText: 'Are you sure you want to logout? This app is awsome so I recommend you to stay.',
-                cancelText: 'Cancel',
-                cancel: function() {},
-                buttonClicked: function(index) {
-                    return true;
-                },
-                destructiveButtonClicked: function(){
-                    $ionicLoading.show({
-                        template: 'Logging out...'
-                    });
-
-                    // Facebook logout
-                    facebookConnectPlugin.logout(function(){
-                            $ionicLoading.hide();
-                            $state.go('welcome');
-                        },
-                        function(fail){
-                            $ionicLoading.hide();
-                        });
-                }
-            });
+    $scope.imageHandle = function(Type) {
+        var options = {
+            quality : 75,
+            destinationType : Camera.DestinationType.DATA_URL,
+            sourceType : Type,
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 300,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: true
         };
+
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            var imgURI = "data:image/jpeg;base64," + imageData;
+            $scope.newblog.img = imageData;
+            $scope.photos.push(imgURI);
+            $('#img-container').css('height','250px')
+        }, function(err) {
+           console.log(err);
+        });
+      }
+
     });
